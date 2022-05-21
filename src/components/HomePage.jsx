561239c41e3e1cf233ccb01fe35a7e94e12080ae
tudorfis <tudor.fis@gmail.com>
2022-05-21 20:23:14 +0300

    .

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,90 +1,65 @@
 import {
   Card,
   Page,
-  Layout,
-  TextContainer,
-  Image,
-  Stack,
-  Link,
-  Heading,
+  EmptyState,
 } from "@shopify/polaris";
 
-import trophyImgUrl from "../assets/home-trophy.png";
-import { ProductsCard } from "./ProductsCard";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ResourcePicker } from "@shopify/app-bridge-react";
+import ProdcutList from "./Product/ProductList";
 
 export function HomePage() {
-  return (
-    <Page 
-      fullWidth
-      primaryAction={{
-        content: 'Select product',
-        onAction: _ => console.log('buton clicked')
-      }}
-    >
-      <Layout>
-        <Layout.Section>
-          <Card sectioned>
-            <Stack
-              wrap={false}
-              spacing="extraTight"
-              distribution="trailing"
-              alignment="center"
-            >
-              <Stack.Item fill>
-                <TextContainer spacing="loose">
-                  <Heading>Nice work on building a Shopify app 🎉</Heading>
-                  <p>
-                    Your app is ready to explore! It contains everything you
-                    need to get started including the{" "}
-                    <Link url="https://polaris.shopify.com/" external>
-                      Polaris design system
-                    </Link>
-                    ,{" "}
-                    <Link url="https://shopify.dev/api/admin-graphql" external>
-                      Shopify Admin API
-                    </Link>
-                    , and{" "}
-                    <Link
-                      url="https://shopify.dev/apps/tools/app-bridge"
-                      external
-                    >
-                      App Bridge
-                    </Link>{" "}
-                    UI library and components.
-                  </p>
-                  <p>
-                    Ready to go? Start populating your app with some sample
-                    products to view and test in your store.{" "}
-                  </p>
-                  <p>
-                    Learn more about building out your app in{" "}
-                    <Link
-                      url="https://shopify.dev/apps/getting-started/add-functionality"
-                      external
-                    >
-                      this Shopify tutorial
-                    </Link>{" "}
-                    📚{" "}
-                  </p>
-                </TextContainer>
-              </Stack.Item>
-              <Stack.Item>
-                <div style={{ padding: "0 20px" }}>
-                  <Image
-                    source={trophyImgUrl}
-                    alt="Nice work on building a Shopify app"
-                    width={120}
-                  />
-                </div>
-              </Stack.Item>
-            </Stack>
-          </Card>
-        </Layout.Section>
-        <Layout.Section secondary>
-          <ProductsCard />
-        </Layout.Section>
-      </Layout>
-    </Page>
-  );
+  const [isOpen, setIsOpen] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [productsId, setProductsId] = useState([]);
+
+  useEffect(() => {
+    const ids = products.map(product => ({ id: product.id }));
+    setProductsId(ids);
+  }, [products])
+
+  function handleProductSelection(payload) {
+    setIsOpen(false);
+    setProducts(payload.selection)
+  }
+
+  return <>
+    <ResourcePicker
+      resourceType="Product"
+      selectMultiple={true}
+      open={isOpen}
+      onCancel={() => setIsOpen(false)}
+      onSelection={handleProductSelection}
+      initialSelectionIds={productsId}      
+    />
+    { products.length ? (
+      <Page 
+        fullWidth
+        title="Product selector"
+        primaryAction={{
+          content: 'Select product',
+          onAction: _ => setIsOpen(true)
+        }}
+      >
+        
+        <ProdcutList
+          products={products}
+        />
+      </Page>
+    ) : (
+      <Card>
+        <EmptyState 
+          heading="Manage the products you want to display"
+          image="https://cdn.shopify.com/s/files/1/0262/4071/2726/files/emptystate-files.png"
+          action={{
+            content: "Select products",
+            onAction: () => setIsOpen(true),
+            onSelection: () => {}
+          }}
+        >
+          <p>Select the products you want to use on your banner</p>
+        </EmptyState>
+      </Card>
+    )}
+  </>
 }
